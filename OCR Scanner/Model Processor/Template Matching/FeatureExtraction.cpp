006230a07925312ae57736107ead1ac7ab4523b2
Minaam Ahmed Awan <MinaamAw@gmail.com>
2021-07-15 17:46:37 +0500

    OpenCV: C++ CNIC Implementation.

--- OCR Scanner/Model Processor/Template Matching/FeatureExtraction.cpp
+++ OCR Scanner/Model Processor/Template Matching/FeatureExtraction.cpp
@@ -2,7 +2,7 @@
  FeatureExtraction.cpp
  OCR Scanner
  
- Created by Minaam Ahmed Awan on 14/07/2021.
+ Created by Minaam Ahmed Awan on 13/07/2021.
  
  Abstract:
  Feature Extraction: Performs Feature Extraction through OpenCV Libraries.
@@ -11,7 +11,32 @@
 
 // Header Files
 #include "FeatureExtraction.hpp"
-//#include <opencv2/opencv.hpp>
+#include "opencv2/core.hpp"
 
 
-// Namespace:
+// Namespaces:
+using namespace cv;
+using namespace std;
+
+
+// Driver Code:
+int test() {
+    
+    Mat color = imread("/Users/minaamawan/Desktop/Test.png");
+    
+    if (color.empty()) {
+        cout << "No File";
+        
+        return 1;
+    }
+    
+    imshow("Display window", color);
+    
+    int k = waitKey(0); // Wait for a keystroke in the window
+    
+    if(k == 's') {
+        imwrite("starry_night.png",color);
+    }
+    
+    return 0;
+}
--- OCR Scanner/Model Processor/Template Matching/FeatureExtraction.hpp
+++ OCR Scanner/Model Processor/Template Matching/FeatureExtraction.hpp
@@ -1,13 +1,23 @@
-//
-//  FeatureExtraction.hpp
-//  OCR Scanner
-//
-//  Created by Minaam Ahmed Awan on 15/07/2021.
-//
+/*
+ FeatureExtraction.hpp
+ OCR Scanner
+ 
+ Created by Minaam Ahmed Awan on 13/07/2021.
+ */
+
 
 #ifndef FeatureExtraction_hpp
 #define FeatureExtraction_hpp
 
+
+// Headers:
 #include <stdio.h>
+#include <opencv2/opencv.hpp>
+
+
+// Namespaces:
+using namespace cv;
+using namespace std;
+
 
 #endif /* FeatureExtraction_hpp */
